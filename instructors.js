f84d915e4c9b28bf04dbc0e700b498eba99f43be
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2020-08-06 04:06:08 +0100

    Gravando dados em Json e  convertendo e Stringify, pegado  a hora actual  com Date.now() e o Date.parse()

--- instructors.js
+++ instructors.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const data = require('./data.json')
+
 exports.post = function(req,res){
   const keys = Object.keys(req.body)
   for(key of keys){
@@ -5,5 +8,20 @@ exports.post = function(req,res){
       return res.send('please, fill all fields')
     }
   } 
-  return res.send(req.body)
+  req.body.Birth = Date.parse(req.body.Birth)
+  req.body.date_Now = Date.now()
+  
+  
+  data.constructors.push(req.body)
+  
+  fs.writeFile('data.json', JSON.stringify(data, null, 2 ), function(err) {
+    if(err){
+      return  res.send("Ouve um erro")
+    }
+    return res.redirect('/instructors')
+   
+  })
+   
+
+ // return res.send(req.body)
 }
